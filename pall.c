c6ab8d67f7fcd8c5410ef9cd00c33cab4982ad5a
root <yasseramr300@gmail.com>
2024-01-18 14:57:42 -0800

    s

--- pall.c
+++ pall.c
@@ -1,28 +1,21 @@
 #include "monty.h"
-
 /**
- * f_pall - Prints the elements of the stack
- * @head: Pointer to the head of the stack
- * @counter: Unused parameter
- *
- * Description: This function prints the elements of the stack.
- * It traverses the stack and prints each element followed by a newline.
- *
- * Return: No return value
- */
+ * f_pall - prints the stack
+ * @head: stack head
+ * @counter: no used
+ * Return: no return
+*/
 void f_pall(stack_t **head, unsigned int counter)
 {
-    stack_t *current;
-    (void)counter;
+	stack_t *h;
+	(void)counter;
 
-    current = *head;
-
-    if (current == NULL)
-        return;
-
-    while (current != NULL)
-    {
-        printf("%d\n", current->n);
-        current = current->next;
-    }
+	h = *head;
+	if (h == NULL)
+		return;
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
 }
--- push.c
+++ push.c
@@ -1,52 +1,37 @@
 #include "monty.h"
-
 /**
- * f_push - Adds a node to the stack
- * @head: Pointer to the head of the stack
- * @counter: Line number
- *
- * Description: This function adds a node to the stack with the value
- * specified in the global variable 'bus.arg'. It checks if the argument
- * is a valid integer and handles errors accordingly. The node is added to
- * the stack or queue based on the value of 'bus.lifi'.
- *
- * Return: No return value
- */
+ * f_push - add node to the stack
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+*/
 void f_push(stack_t **head, unsigned int counter)
 {
-    int n, j = 0, flag = 0;
+	int n, j = 0, flag = 0;
 
-    if (bus.arg)
-    {
-        if (bus.arg[0] == '-')
-            j++;
-        for (; bus.arg[j] != '\0'; j++)
-        {
-            if (bus.arg[j] > '9' || bus.arg[j] < '0')
-                flag = 1;
-        }
-        if (flag == 1)
-        {
-            fprintf(stderr, "L%d: usage: push integer\n", counter);
-            fclose(bus.file);
-            free(bus.content);
-            free_stack(*head);
-            exit(EXIT_FAILURE);
-        }
-    }
-    else
-    {
-        fprintf(stderr, "L%d: usage: push integer\n", counter);
-        fclose(bus.file);
-        free(bus.content);
-        free_stack(*head);
-        exit(EXIT_FAILURE);
-    }
-
-    n = atoi(bus.arg);
-
-    if (bus.lifi == 0)
-        addnode(head, n);
-    else
-        addqueue(head, n);
+	if (bus.arg)
+	{
+		if (bus.arg[0] == '-')
+			j++;
+		for (; bus.arg[j] != '\0'; j++)
+		{
+			if (bus.arg[j] > 57 || bus.arg[j] < 48)
+				flag = 1; }
+		if (flag == 1)
+		{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+			fclose(bus.file);
+			free(bus.content);
+			free_stack(*head);
+			exit(EXIT_FAILURE); }}
+	else
+	{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE); }
+	n = atoi(bus.arg);
+	if (bus.lifi == 0)
+		addnode(head, n);
+	else
+		addqueue(head, n);
 }
